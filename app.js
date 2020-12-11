400bf6b022d905cc64f0570e09a0415069b51eff
CodyBryson <codybryson@hotmail.com>
2020-12-10 17:11:00 -0700

    updates

--- app.js
+++ app.js
@@ -18,18 +18,68 @@ let attackType = {
   }
 }
 //enemy type object dictionary
-let enemyType = {
-  zangief: {
+let enemyType = [
+  {
     name: 'Zangief',
-    health: 250,
-    img: '<img src="Zangiefrender.png" alt="" class="img-fluid" srcset=""></img>'
+    health: 350,
+    img: '<img src="Zangiefrender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 1
   },
-  bison: {
+  {
     name: 'M. Bison',
+    health: 200,
+    img: '<img src="Bisonrender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 2
+  },
+  {
+    name: 'Akuma',
+    health: 275,
+    img: '<img src="Akumarender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 3
+  },
+  {
+    name: 'Cammy',
+    health: 150,
+    img: '<img src="Cammyrender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 4
+  },
+  {
+    name: 'Chun-Li',
     health: 175,
-    img: '<img src="Bisonrender.png" alt="" class="img-fluid" srcset=""></img>'
+    img: '<img src="Chunrender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 5
+  },
+  {
+    name: 'E.Honda',
+    health: 375,
+    img: '<img src="Ehonda.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 6
+  },
+  {
+    name: 'Ryu',
+    health: 50,
+    img: '<img src="Ryurender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 7
+  },
+  {
+    name: 'Sagat',
+    health: 200,
+    img: '<img src="Sagatclean.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 8
+  },
+  {
+    name: 'Ken',
+    health: 75,
+    img: '<img src="Kenrender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 9
+  },
+  {
+    name: 'Ibuki',
+    health: 250,
+    img: '<img src="Ibukirender.png" alt="" class="img-fluid" srcset=""></img>',
+    id: 10
   }
-}
+]
 
 
 
@@ -60,13 +110,8 @@ function drawHitCounter() {
 
 //call random enemy
 function randomEnemy() {
-  let index = Math.floor(Math.random() * 2)
-  if (index < 1) {
-    enemy = enemyType.zangief
-  }
-  else {
-    enemy = enemyType.bison
-  }
+  let index = Math.floor(Math.random() * 10)
+  enemy = enemyType[index]
 
   drawEnemy()
 
